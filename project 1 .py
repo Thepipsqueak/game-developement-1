1758d70350cb8a54c18b2805cf52a81fec0ef830
Thepipsqueak <karl.pmatta@gmail.com>
2025-07-20 19:12:09 +0200

    Update project 1 .py

--- project 1 .py
+++ project 1 .py
@@ -6,4 +6,10 @@ HEIGHT=350
 def draw():
     rec=Rect((225,175),(200,50))
     screen.draw.rect(rec,"turquoise")
+    re=Rect((125,100),(200,50))
+    screen.draw.rect(re,"purple")
+    r=Rect((75,200),(200,50))
+    screen.draw.rect(r,"lime green")
+    rectan=Rect((25,50),(200,50))
+    screen.draw.rect(rectan,"white")
 pgzrun.go()
