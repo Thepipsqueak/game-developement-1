0ac16a8e239456fa8a603b479f185ee948d44a6b
Thepipsqueak <karl.pmatta@gmail.com>
2025-07-27 20:00:04 +0200

    Add files via upload

---
+++ wack a baby yoda/wack a baby yoda .py
@@ -0,0 +1,25 @@
+import pgzrun
+import random
+WIDTH=800
+HEIGHT=500
+mes=""
+# creating an actor
+mot=Actor("cutie pie.png")
+def draw():
+    screen.blit("try to find me heeheehee",(0,0))
+    screen.draw.text(mes,center=(400,50),fontsize=25)
+    mot.draw()
+def update():
+    pass
+def stuti():
+    mot.x=random.randint(0,800)
+    mot.y=random.randint(0,500)
+stuti()
+#mouse event
+def on_mouse_down(pos):
+    global mes
+    if mot.collidepoint(pos):
+        stuti()
+        mes="You found him!!!but...can you do it again??"
+
+pgzrun.go()
