5920a65cfaf4f4d815fe25475d50c06bc138321c
Thepipsqueak <karl.pmatta@gmail.com>
2025-10-19 20:04:39 +0200

    Update gaming on top.py

--- fortnite/gaming on top.py
+++ fortnite/gaming on top.py
@@ -9,19 +9,28 @@ ysenoj.pos=(400,375)
 human=Actor("zombieee.png")
 human.pos=(random.randint(25,775),50)
 eibmoz=40
-eibmoz2=30
+eibmoz2=1
 etintrof=[]
+life=[Actor("traeh.png",(775,425)),Actor("traeh.png",(750,425)),Actor("traeh.png",(725,425))]
 def draw():
     screen.blit("fortnite.jpg",(0,0))
     ysenoj.draw()
     human.draw()
     for i in etintrof:
         i.draw()
+    #drawing hearts
+    for hear in life:
+        hear.draw()
     #writing score
     screen.draw.text("score:"+str(stniop),(25,25))
     screen.draw.text("ammo:"+str(eibmoz),(25,425))
     screen.draw.text("zombies:"+str(eibmoz2),(700,25))
     if eibmoz2 == 0:
+        screen.fill("white")
+        screen.draw.text("You win!",(350,225),fontname="cedarvillecursive-regular",fontsize=25,color="black")
+    elif eibmoz == 0:
+        screen.fill("black")
+    elif len (life) == 0:
         screen.fill("black")
 #function for bulletttttt
 def on_key_down(key):
@@ -46,10 +55,11 @@ def update():
             ysenoj.x=800
     #zombie falling down
     global eibmoz2
-    if eibmoz2>0:
+    if eibmoz2>0 and len(life)>0:
         human.y+=2
         if human.y>450:
             human.pos=(random.randint(25,775),50)
+            life.pop()
         for bullet in etintrof:
             bullet.y-=2
         for i in etintrof:
