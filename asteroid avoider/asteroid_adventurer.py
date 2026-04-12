18b11d11c6d80923d9faf2305c8b2aaf28f25f87
Thepipsqueak <karl.pmatta@gmail.com>
2026-04-12 19:54:59 +0200

    Update asteroid_adventurer.py

--- asteroid avoider/asteroid_adventurer.py
+++ asteroid avoider/asteroid_adventurer.py
@@ -6,6 +6,8 @@ screen=pygame.display.set_mode((800,450))
 utahime=pygame.image.load("whoaaaa.jpg")
 iranfromhome_love=True
 shark=pygame.image.load("asteroid.png")
+jared=pygame.image.load("innocent_child_Jared.png")
+acneedbob=pygame.image.load("bob.png")
 class two (pygame.sprite.Sprite):
     def __init__(self,image,x,y):
         super().__init__()
@@ -17,10 +19,29 @@ class two (pygame.sprite.Sprite):
         self.rect.x+=sonic
     def carré(self,flash):
         self.rect.y+=flash
+#class for bob and innocent child jared
+class innocents(pygame.sprite.Sprite):
+    def __init__(self,image,x,y):
+        super().__init__()
+        self.image=image
+        self.rect=self.image.get_rect()
+        self.rect.x=x
+        self.rect.y=y
+
+
 mmi=two(shark,350,350)
+ex=random.randint(50,750)
+undeservedDEATH=innocents(jared,ex,50)
+xe=random.randint(50,750)
+inloveBOB=innocents(acneedbob,xe,50)
 troupe=pygame.sprite.Group()
 troupe.add(mmi)
+InnocentMassacre=pygame.sprite.Group()
+InnocentMassacre.add(undeservedDEATH)
+InnocentMassacre.add(inloveBOB)
+clock=pygame.time.Clock()
 while iranfromhome_love:
+    clock.tick(60)
     screen.blit(utahime,(0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -28,15 +49,15 @@ while iranfromhome_love:
     #key movement
     keys=pygame.key.get_pressed()
     if keys[K_w]:
-        mmi.carré(-0.5)
+        mmi.carré(-2)
     if keys[K_s]:
-        mmi.carré(0.5)
+        mmi.carré(2)
     if keys[K_d]:
-        mmi.losange(1)
+        mmi.losange(2)
     if keys[K_a]:
-        mmi.losange(-1)
+        mmi.losange(-2)
     troupe.draw(screen)
-    
+    InnocentMassacre.draw(screen)
 
     pygame.display.update()
 pygame.quit()
