f570c14c749e1b526728bde7e18713a1dd97907f
Thepipsqueak <karl.pmatta@gmail.com>
2026-04-19 19:57:45 +0200

    Update asteroid_adventurer.py

--- asteroid avoider/asteroid_adventurer.py
+++ asteroid avoider/asteroid_adventurer.py
@@ -27,19 +27,32 @@ class innocents(pygame.sprite.Sprite):
         self.rect=self.image.get_rect()
         self.rect.x=x
         self.rect.y=y
+        self.speed=random.randint(3,5)
+    def tempest(self):
+        self.rect.y+=self.speed
 
 
+InnocentMassacre=pygame.sprite.Group()
 mmi=two(shark,350,350)
-ex=random.randint(50,750)
-undeservedDEATH=innocents(jared,ex,50)
-xe=random.randint(50,750)
-inloveBOB=innocents(acneedbob,xe,50)
+for i in range(7):
+    ex=random.randint(50,750)
+    why=random.randint(-300,-50)
+    stuti=random.choice([jared,acneedbob])
+    undeservedDEATH=innocents(stuti,ex,why)
+    InnocentMassacre.add(undeservedDEATH)
 troupe=pygame.sprite.Group()
 troupe.add(mmi)
-InnocentMassacre=pygame.sprite.Group()
-InnocentMassacre.add(undeservedDEATH)
-InnocentMassacre.add(inloveBOB)
 clock=pygame.time.Clock()
+#beginning screen
+crescent=pygame.font.Font(None,100)
+for number in ["3","2","1"]:
+    screen.fill("white")
+    text=crescent.render(number,True,("black"))
+    rect=text.get_rect(center=(400,225))
+    screen.blit(text,rect)
+    pygame.display.update()
+    pygame.time.delay(1000)
+
 while iranfromhome_love:
     clock.tick(60)
     screen.blit(utahime,(0,0))
@@ -49,13 +62,21 @@ while iranfromhome_love:
     #key movement
     keys=pygame.key.get_pressed()
     if keys[K_w]:
-        mmi.carré(-2)
+        mmi.carré(-5)
     if keys[K_s]:
-        mmi.carré(2)
+        mmi.carré(5)
     if keys[K_d]:
-        mmi.losange(2)
+        mmi.losange(5)
     if keys[K_a]:
-        mmi.losange(-2)
+        mmi.losange(-5)
+    #innocents falling down
+    for i in InnocentMassacre:
+        i.tempest()
+        if i.rect.y>450:
+            i.rect.y=random.randint(-300,-50)
+            i.rect.x=random.randint(50,750)
+            i.speed=random.randint(3,5)
+
     troupe.draw(screen)
     InnocentMassacre.draw(screen)
 
