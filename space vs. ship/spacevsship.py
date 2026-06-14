88a57abd873a640982641a25caa1a7ed59ef7f76
Thepipsqueak <karl.pmatta@gmail.com>
2026-06-14 19:54:04 +0200

    Update spacevsship.py

--- space vs. ship/spacevsship.py
+++ space vs. ship/spacevsship.py
@@ -3,6 +3,7 @@ import pygame
 pygame.init()
 screen=pygame.display.set_mode((800,547))
 bg=pygame.image.load("images/bg.jpg")
+cat=3
 class Space (pygame.sprite.Sprite):
     def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
@@ -11,16 +12,22 @@ class Space (pygame.sprite.Sprite):
         self.rect.center=[x,y]
     def update(self):
         key=pygame.key.get_pressed()
-        if key[pygame.K_LEFT]:
-            self.rect.x-=1
-        if key[pygame.K_RIGHT]:
-            self.rect.x+=1
+        if key[pygame.K_LEFT] and self.rect.x>0:
+            self.rect.x-=5
+        if key[pygame.K_RIGHT] and self.rect.x<730:
+            self.rect.x+=5
 class Enemy (pygame.sprite.Sprite):
     def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
         self.image=pygame.image.load("images/red.png")
         self.rect=self.image.get_rect()
         self.rect.center=[x,y]
+    def update(self):
+        global cat
+        self.rect.y+=1
+        if self.rect.y>547:
+            cat-=1
+            self.kill()
 universe=Space(400,500)
 universegroup=pygame.sprite.Group()
 universegroup.add(universe)
@@ -28,7 +35,9 @@ reverse=pygame.sprite.Group()
 for i in range (5):
     read=Enemy(i*160+80,47)
     reverse.add(read)
+clock=pygame.time.Clock()
 while True:
+    clock.tick(60)
     screen.blit(bg,(0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -36,4 +45,5 @@ while True:
     universegroup.draw(screen)
     universegroup.update()
     reverse.draw(screen)
+    reverse.update()
     pygame.display.update()
